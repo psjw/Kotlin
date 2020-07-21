b8171d1f2f6ce172e9e8fbbd9832456d52600ca7
lks0108 <dlrudtn108@gmail.com>
2020-07-22 00:06:16 +0900

    al: not a git repository (or any of the parent directories): .git

--- app/src/main/java/com/example/myapplication/Kotlin/04.Function.kt
+++ app/src/main/java/com/example/myapplication/Kotlin/04.Function.kt
@@ -11,23 +11,70 @@ package com.example.myapplication.Kotlin
 //      return 반환값
 // }
 
-fun plus(first : Int, second : Int) : Int{
+fun plus(first: Int, second: Int): Int {
     println(first)
     println(second)
-    val result : Int = first + second
+    val result: Int = first + second
     println(result)
-    return  result
+    return result
 }
 
-fun main(array : Array<String>){
+// -디폴트 값을 갖는 함수 만들기
+fun plusFive(first: Int, second: Int = 5): Int {
+    val result: Int = first + second
+    return result
+}
+
+// -반환 값이 없는 함수 만들기(1)
+// Unit 생략 가능
+fun printPlus(first: Int, second: Int): Unit {
+    val result: Int = first + second
+    println(result)
+}
+
+// -반환 값이 없는 함수 만들기(2)
+fun printPlus2(first: Int, second: Int) {
+    val result: Int = first + second
+    println(result)
+}
+
+//- 간단하게 함수를 선언
+fun plusShort(first: Int, second: Int) = first + second
+
+//- 가변인자를 갖는 함수 선언하는 방법
+//1개 부터 n개까지 인수 받을수있음
+fun plusMany(vararg numbers: Int) {
+    for (number in numbers) {
+        println(number)
+    }
+}
+
+fun main(array: Array<String>) {
     //함수를 호출하는 방법
-    var result=plus(5,10)
+    var result = plus(5, 10)
     println(result)
     //인수를 명시적으로 전달하는 방법
-    var result2=plus(first=20,second=30)
+    var result2 = plus(first = 20, second = 30)
     println(result2)
 
-    var result3=plus(second=100,first=10)
+    var result3 = plus(second = 100, first = 10)
     println(result3)
 
+    //디폴트 값을 갖는 함수 호출하기
+    println()
+    val result4 = plusFive(10, 20)
+    println(result4)
+    var result5 = plusFive(10)
+    println(result5)
+
+    println()
+    printPlus(10, 20)
+
+    println()
+    var result6 = plusShort(50, 50)
+    println(result6)
+
+    println()
+    plusMany(1,2,3)
+    plusMany(100)
 }
